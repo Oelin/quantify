15fd10776c22aed53bfd1675470ad6d5f25febaf
Oelin <42823429+Oelin@users.noreply.github.com>
2021-12-04 19:37:57 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-export default domain => {
-    const exists = predicate => {
-        for (let element of domain) {
-            if (predicate(element)) return true
-        }
-        return false
+module.exports = set => {
+
+    const some = predicate => {
+ 
+	    for (let element of set) {
+	        if (predicate(element)) return true
+	    }
+
+	    return false
     }
-    const all = predicate => !exists(x => !predicate(x))
-    
-    return { all, exists }
+
+    const all = predicate => !some(x => !predicate(x))
+
+    return { some, all }
 }
